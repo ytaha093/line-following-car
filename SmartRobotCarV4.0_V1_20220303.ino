3507edb7e6c869f5da06aa4f5cd591f15f5e5a77
Yaseen <yaseen_taha123@hotmail.com>
2024-04-02 17:14:57 -0600

    sorta working

---
+++ SmartRobotCarV4.0_V1_20220303.ino
@@ -0,0 +1,32 @@
+#include <avr/wdt.h>
+#include "ApplicationFunctionSet_xxx0.h"
+
+void setup()
+{
+  Application_FunctionSet.ApplicationFunctionSet_Init();
+  wdt_enable(WDTO_2S);
+}
+
+void loop()
+{
+  wdt_reset();
+  Application_FunctionSet.ApplicationFunctionSet_SensorDataUpdate();
+  Application_FunctionSet.ApplicationFunctionSet_KeyCommand();
+  Application_FunctionSet.ApplicationFunctionSet_RGB();
+  Application_FunctionSet.ApplicationFunctionSet_Follow();
+  Application_FunctionSet.ApplicationFunctionSet_Obstacle();
+  Application_FunctionSet.ApplicationFunctionSet_Tracking();
+  Application_FunctionSet.ApplicationFunctionSet_Rocker();
+  Application_FunctionSet.ApplicationFunctionSet_Standby();
+  Application_FunctionSet.ApplicationFunctionSet_IRrecv();
+  Application_FunctionSet.ApplicationFunctionSet_SerialPortDataAnalysis();
+
+  Application_FunctionSet.CMD_ServoControl_xxx0();
+  Application_FunctionSet.CMD_MotorControl_xxx0();
+  Application_FunctionSet.CMD_CarControlTimeLimit_xxx0();
+  Application_FunctionSet.CMD_CarControlNoTimeLimit_xxx0();
+  Application_FunctionSet.CMD_MotorControlSpeed_xxx0();
+  Application_FunctionSet.CMD_LightingControlTimeLimit_xxx0();
+  Application_FunctionSet.CMD_LightingControlNoTimeLimit_xxx0();
+  Application_FunctionSet.CMD_ClearAllFunctions_xxx0();
+}
